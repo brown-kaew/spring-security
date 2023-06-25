4119bd15f3a305b5577b996c6000bc22ea8ec187
Kaew Tibkham <kaewtibkham@gmail.com>
2023-06-25 18:06:05 +0700

    configure authentication

--- src/main/kotlin/brown/kaew/security/config/ApplicationConfig.kt
+++ src/main/kotlin/brown/kaew/security/config/ApplicationConfig.kt
@@ -3,8 +3,14 @@ package brown.kaew.security.config
 import brown.kaew.security.user.UserRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 class ApplicationConfig(
@@ -16,5 +22,23 @@ class ApplicationConfig(
         return UserDetailsService { username ->
             userRepository.findByEmail(username) ?: throw UsernameNotFoundException("User not found")
         }
+    }
+
+    @Bean
+    fun authenticationProvider(): AuthenticationProvider {
+        val authProvider = DaoAuthenticationProvider()
+        authProvider.setUserDetailsService(userDetailsService())
+        authProvider.setPasswordEncoder(passwordEncoder())
+        return authProvider
+    }
+
+    @Bean
+    fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager {
+        return config.authenticationManager
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
     }
 }
