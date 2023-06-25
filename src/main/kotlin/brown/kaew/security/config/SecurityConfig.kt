2e855434b94c0c8fa0b062c888d60292818e85a9
Kaew Tibkham <kaewtibkham@gmail.com>
2023-06-25 18:05:30 +0700

    Add SecurityConfig

---
+++ src/main/kotlin/brown/kaew/security/config/SecurityConfig.kt
@@ -0,0 +1,38 @@
+package brown.kaew.security.config
+
+import jakarta.servlet.Filter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+        val jwtAuthFilter: JwtAuthenticationFilter,
+        val authenticationProvider: AuthenticationProvider,
+) {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
+
+        return http.build()
+    }
+}
